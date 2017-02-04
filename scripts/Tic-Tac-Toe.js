49a9cd02a2175f51adaa8a497261c92b3fc04699
Lasse Zaar <Lasse.Zaar@gmail.com>
2017-02-04 18:42:45 +0100

    Square tiles in place
    Still need alignment of tiles.

--- scripts/Tic-Tac-Toe.js
+++ scripts/Tic-Tac-Toe.js
@@ -1,86 +1,89 @@
 var painted;
-        var content;
-        var winningCombinations;
-        var turn = 0;
-        var theCanvas;
-        var c;
-        var cxt;
-        var squaresFilled = 0;
-        var w;
-        var y;
+var content;
+var winningCombinations;
+var turn = 0;
+var theCanvas;
+var c;
+var cxt;
+var squaresFilled = 0;
+var w;
+var y;
 
-        //Instanciate Arrays
-        window.onload = function(){
-            painted = new Array();
-            content = new Array();
-            winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+//Instanciate Arrays
+window.onload = function() {
+    painted = new Array();
+    content = new Array();
+    winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-            for(var l = 0; l <= 8; l++){
-            painted[l] = false;
-            content[l]='';
-            }
-        }  
+    for (var l = 0; l <= 8; l++) {
+        painted[l] = false;
+        content[l] = '';
+    }
+}
 
-        function restartClicked(){
-            location.reload();
+function restartClicked() {
+    location.reload();
+}
+
+//Game methods
+function canvasClicked(canvasNumber) {
+    theCanvas = "canvas" + canvasNumber;
+    c = document.getElementById(theCanvas);
+    cxt = c.getContext("2d");
+
+    if (painted[canvasNumber - 1] == false) {
+        if (turn % 2 == 0) {
+            cxt.beginPath();
+            cxt.moveTo(width, height);
+            cxt.lineTo(40, 40);
+            cxt.moveTo(40, 10);
+            cxt.lineTo(10, 40);
+            cxt.stroke();
+            cxt.closePath();
+            content[canvasNumber - 1] = 'X';
+        } else {
+            cxt.beginPath();
+            cxt.arc(25, 25, 20, 0, Math.PI * 2, true);
+            cxt.stroke();
+            cxt.closePath();
+            content[canvasNumber - 1] = 'O';
         }
 
-        function helpClicked(){
-            
+        turn++;
+        painted[canvasNumber - 1] = true;
+        squaresFilled++;
+        checkForWinners(content[canvasNumber - 1]);
+
+        if (squaresFilled == 9) {
+            alert("THE GAME IS OVER!");
+            location.reload(true);
         }
+    } else {
+        alert("THAT SPACE IS ALREADY OCCUPIED.");
+    }
+}
 
-        //Game methods
-        function canvasClicked(canvasNumber){
-            theCanvas = "canvas"+canvasNumber;
-            c = document.getElementById(theCanvas);
-            cxt = c.getContext("2d");
-
-            if(painted[canvasNumber-1] ==false){
-                if(turn%2==0){
-                    cxt.beginPath();
-                    cxt.moveTo(10,10);
-                    cxt.lineTo(40,40);
-                    cxt.moveTo(40,10);
-                    cxt.lineTo(10,40);
-                    cxt.stroke();
-                    cxt.closePath();
-                    content[canvasNumber-1] = 'X';
-                }
-                else{
-                    cxt.beginPath();
-                    cxt.arc(25,25,20,0,Math.PI*2,true);
-                    cxt.stroke();
-                    cxt.closePath();
-                    content[canvasNumber-1] = 'O';
-                }
-
-                turn++;
-                painted[canvasNumber-1] = true;
-                squaresFilled++;
-                checkForWinners(content[canvasNumber-1]);
-
-                if(squaresFilled==9){
-                    alert("THE GAME IS OVER!");
-                    location.reload(true);
-                }
-            }
-            else{
-                alert("THAT SPACE IS ALREADY OCCUPIED.");
-            }
+function checkForWinners(symbol) {
+    for (var a = 0; a < winningCombinations.length; a++) {
+        if (content[winningCombinations[a][0]] == symbol && content[winningCombinations[a][1]] == symbol && content[winningCombinations[a][2]] == symbol) {
+            alert(symbol + " WON!");
+            playAgain();
         }
+    }
+}
 
-        function checkForWinners(symbol){        
-            for(var a = 0; a < winningCombinations.length; a++){
-                if(content[winningCombinations[a][0]]==symbol&&content[winningCombinations[a][1]]==	symbol&&content[winningCombinations[a][2]]==symbol){
-                    alert(symbol+ " WON!");
-                    playAgain();
-                }
-            }
-        }
-
-        function playAgain(){
-            y=confirm("PLAY AGAIN?");
-            if(y==true){
-                location.reload(true);
-            }
-        }
+function playAgain() {
+    y = confirm("PLAY AGAIN?");
+    if (y == true) {
+        location.reload(true);
+    }
+}
